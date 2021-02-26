392cf116e3d93a5b6d55c0979673282bebd78b7e
coreyg142 <coreyg142@gmail.com>
2021-02-25 22:48:21 -0500

    Start of a Discord.py bot
    Signed-off-by: coreyg142 <coreyg142@gmail.com>

---
+++ Discord.py
@@ -0,0 +1,20 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+
+def main():
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+
+    client = discord.Client()
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} has connected to Discord!')
+
+    client.run(TOKEN)
+
+
+if __name__ == '__main__':
+    main()
